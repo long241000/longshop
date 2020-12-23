7380ee4e99e1498e9490a3167ee30b412f612e31
long241000 <74395113+long241000@users.noreply.github.com>
2020-12-23 12:20:20 +0700

    Add files via upload

---
+++ configs/config_DB.js
@@ -0,0 +1,16 @@
+module.exports = {
+    localdb: {
+        urldb : 'mongodb+srv://admin:admin@cluster0.vnn7e.mongodb.net/long12?retryWrites=true&w=majority',
+        dbname : "long12",
+        dbusername : "",
+        dbpassword : "",
+    },
+
+    clouddb: {
+        urldb : 'mongodb+srv://admin:admin@cluster0.vnn7e.mongodb.net/long12?retryWrites=true&w=majority',
+        dbname : "long12",
+        dbusername : "",
+        dbpassword : "",
+    }
+
+};
